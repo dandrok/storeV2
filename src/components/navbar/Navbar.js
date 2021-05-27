416721c00cf20142f075bec5e2e64c551aa00bc0
dandrok <patryk.stusik@gmail.com>
2021-05-27 20:41:06 +0200

    add material

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: 3,
+    border: 0,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  },
+  label: {
+    textTransform: 'capitalize',
+  },
+})
 
 const Navbar = () => {
+  const classes = useStyles()
   return (
     <div>
-      <p>hej</p>
+      <Button
+        classes={{
+          root: classes.root, // class name, e.g. `classes-nesting-root-x`
+          label: classes.label, // class name, e.g. `classes-nesting-label-x`
+        }}
+      >
+        classes nesting
+      </Button>
+      <form className={classes.container} noValidate>
+        <TextField
+          id='date'
+          label='Birthday'
+          type='date'
+          defaultValue='2017-05-24'
+          className={classes.textField}
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+      </form>
     </div>
   )
 }
